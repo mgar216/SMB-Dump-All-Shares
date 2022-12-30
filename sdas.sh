3ae351b062b05f95b80c2a309c1f63d4279e0b7f
mgar216 <28987804+mgar216@users.noreply.github.com>
2022-12-30 10:15:04 -0700

    Update sdas.sh

--- sdas.sh
+++ sdas.sh
@@ -14,30 +14,47 @@ else
     echo -e -n "\n"
     [[ -d "smb_dump" ]] || mkdir "smb_dump" 2> /dev/null
     cd "smb_dump"
+    smbmap_cmd="-H $1"
 fi
 
 if [[ -z $2 ]];
 then
     username=""
+    smbmap_cmd="$smbmap_cmd -u 'a'"
 else
     username="$2"
     echo -e "${GREEN}[*]${NC} Using Username: ${GREEN}$username${NC}"
     echo -e -n "\n"
+    smbmap_cmd="$smbmap_cmd -u $2"
 fi
 
 if [[ -z $3  ]];
 then
     password=""
+    smbmap_cmd="$smbmap_cmd -p 'a'"
 else
     password="$3"
     echo -e "${GREEN}[*]${NC} Using Password: ${GREEN}$password${NC}"
     echo -e -n "\n"
+    smbmap_cmd="$smbmap_cmd -p $3"
 fi
 
 
 all_shares=$(smbclient -L //"$1" -U="$username" --password="$password" 2> /dev/null | awk '{ print $1 }' | tail -n+4 | head -n+2)
-echo -e "${GREEN}[*]${NC} Dumping all Shares for: ${GREEN}$1${NC}"
+
+echo -e "${GREEN}[+]${NC} Found the following Shares:"
+for i in $all_shares; do
+    echo -e "\t//$1/${GREEN}$i${NC}"
+done
 echo -e -n "\n"
+echo -e "${GREEN}[+]${NC} Checking READ/WRITE Permissions:"
+smbmap=$(smbmap $smbmap_cmd | tail -n+2)
+echo -e "${GREEN}$smbmap${NC}"
+echo -e -n "\n"
+
+echo -e "${GREEN}[*]${NC} Dumping all available Shares for: ${GREEN}$1${NC}"
+echo -e -n "\n"
+
 for i in $all_shares; do
     [[ -d "$i" ]] || mkdir "$i" 2> /dev/null
     cd "$i"
